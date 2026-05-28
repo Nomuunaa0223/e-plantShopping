73d1783f8d5365355b7d754aa778626b45fb7c4a
Nomuunaa <nomunabaterdene7@gmail.com>
2026-05-28 13:49:07 +0800

    Add AboutUs component for nursery information

---
+++ src/AboutUs.jsx
@@ -0,0 +1,11 @@
+export default function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a houseplant shop that helps customers bring nature
+        into their homes with beautiful indoor plants.
+      </p>
+    </div>
+  );
+}
